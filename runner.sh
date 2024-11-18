dafc2b29dd59efb159619052ad2597e280ec2a1c
20120472 <hienthai.030102@gmail.com>
2024-11-18 14:18:26 +0700

    Remove printing lines. Done

--- runner.sh
+++ runner.sh
@@ -4,10 +4,7 @@ JAR_FILE="target/hotel-merger-1.0.jar"
 
 # Check if the JAR file exists
 if [ ! -f "$JAR_FILE" ]; then
-  echo "JAR file not found. Building the project..."
   mvn clean package
-else
-  echo "JAR file found. Skipping build stage..."
 fi
 
 # Run the project
